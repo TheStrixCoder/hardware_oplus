58954346601c73054ab96166b4d50516889150b4
hurtcopain <jacksummers715@gmail.com>
2025-01-12 00:01:26 -0500

    remove for now, will properly add pocketjudge later

--- KeyHandler/src/org/lineageos/settings/device/KeyHandler.kt
+++ KeyHandler/src/org/lineageos/settings/device/KeyHandler.kt
@@ -145,10 +145,6 @@ class KeyHandler(private val context: Context) : DeviceKeyHandler {
         context.sendBroadcastAsUser(intent, UserHandle(UserHandle.USER_CURRENT))
     }
 
-    override fun onPocketStateChanged(inPocket: Boolean) {
-        // do nothing
-    }
-
     private fun setZenMode(zenMode: Int) {
         // Set zen mode
         notificationManager.setZenMode(zenMode, null, TAG)
