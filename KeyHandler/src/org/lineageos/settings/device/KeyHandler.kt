9e1c0717402b888d8a4b07d01e7b5a57d2c83adb
theStrixCoder <mailme.bidyadhar123@gmail.com>
2025-02-22 13:45:48 +0530

    remove onPocketStateChanged for now

--- KeyHandler/src/org/lineageos/settings/device/KeyHandler.kt
+++ KeyHandler/src/org/lineageos/settings/device/KeyHandler.kt
@@ -171,9 +171,9 @@ class KeyHandler(private val context: Context) : DeviceKeyHandler {
         return null
     }
 
-    override fun onPocketStateChanged(inPocket: Boolean) {
+//    override fun onPocketStateChanged(inPocket: Boolean) {
         // Do nothing
-    }
+//    }
 
 
     private fun doHapticFeedback(effect: VibrationEffect?) {
